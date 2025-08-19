4eded0fef504184ea70605701d2d7d77f6e32a72
Nico Billiere <nico.billiere@gmail.com>
2025-08-19 21:38:09 +0400

    V3.1

--- vite.config.mjs
+++ vite.config.mjs
@@ -2,13 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import { fileURLToPath, URL } from "node:url";
 
-// base conditionnelle : / en dev, /portfolio-nicolas/ sur GitHub Pages
 export default defineConfig(({ mode }) => ({
     base: mode === "production" ? "/portfolio-nicolas/" : "/",
     plugins: [react()],
     resolve: {
         alias: {
-            "@": fileURLToPath(new URL("./src", import.meta.url)), // <= alias @ -> src
+            "@": fileURLToPath(new URL("./src", import.meta.url)),
         },
     },
     server: {
@@ -16,6 +15,13 @@ export default defineConfig(({ mode }) => ({
             "/api": {
                 target: "http://localhost:4000",
                 changeOrigin: true,
+            },
+        },
+    },
+    css: {
+        preprocessorOptions: {
+            scss: {
+                api: "modern",
             },
         },
     },
